8e963ed391aea8848fd4ff312f57d79762981208
DanielKremes <danielkremes05@gmail.com>
2023-10-24 19:00:28 -0300

    second commit

--- teste/script.js
+++ teste/script.js
@@ -1,2 +1,4 @@
+console.log("********************************")
 console.log("starting javascript")
 console.log("dio programming logical challenge")
+console.log("********************************")
